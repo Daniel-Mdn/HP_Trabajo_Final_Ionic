5b211b5ff76a1c7eed58be3f681d2d9b45fc3116
Daniel-Mdn <danielmedina012@gmail.com>
2022-08-16 23:20:39 -0300

    modal adicionales added

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -12,3 +12,15 @@ export enum TamaniosHamburguesa{
     Simple="simple",
     Doble="doble"
 }
+export enum extrasHamburguesas{
+    uno="1",
+    dos="2",
+    tres="3",
+}
+export interface Extras{
+    id:string,
+    cantidad:number,
+    precio:number,
+    descExtra:string,
+    idLineaDePed:number
+}
--- src/app/pages/producto/producto.page.ts
+++ src/app/pages/producto/producto.page.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { IonModal } from '@ionic/angular';
-import { OverlayEventDetail } from '@ionic/core';
-import { Tamanios, TamaniosHamburguesa } from 'src/app/constants/constants';
+import { OverlayEventDetail, RangeCustomEvent, RangeValue } from '@ionic/core';
+import {
+  Extras,
+  extrasHamburguesas,
+  Tamanios,
+  TamaniosHamburguesa,
+} from 'src/app/constants/constants';
 import { IProducto } from 'src/app/constants/interfaces';
 import { ProductoService } from 'src/app/services/producto/producto.service';
 
@@ -25,13 +30,45 @@ export class ProductoPage implements OnInit {
     idCat: 1,
     tamanio: Tamanios.Simple,
     descProd:
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
   };
   @ViewChild(IonModal) modal: IonModal;
   tamaniosHamburguesa: TamaniosHamburguesa[] = [
     TamaniosHamburguesa.Doble,
     TamaniosHamburguesa.Simple,
   ];
+  extraHamburguesas: extrasHamburguesas[] = [
+    extrasHamburguesas.uno,
+    extrasHamburguesas.dos,
+    extrasHamburguesas.tres,
+  ];
+  extras: Extras[] = [
+    {
+      id: '1',
+      cantidad: 1,
+      precio: 100,
+      descExtra: 'Panceta',
+      idLineaDePed: 1,
+    },
+    {
+      id: '2',
+      cantidad: 1,
+      precio: 100,
+      descExtra: 'Queso Cheddar',
+      idLineaDePed: 1,
+    },
+    {
+      id: '3',
+      cantidad: 1,
+      precio: 100,
+      descExtra: 'Queso Azul',
+      idLineaDePed: 1,
+    },
+  ];
+  rangeValue: RangeValue;
+  name: string;
+  subtotal: number=0;
+  total: number=0;
   ngOnInit() {
     this.productService
       .getProduct(this.route.snapshot['id'])
@@ -41,8 +78,6 @@ export class ProductoPage implements OnInit {
         }
       });
   }
-
-  name: string;
 
   cancel() {
     this.modal.dismiss(null, 'cancel');
@@ -58,5 +93,9 @@ export class ProductoPage implements OnInit {
       this.name = `Hello, ${ev.detail.data}!`;
     }
   }
+  onIonChange(ev: Event) {
+    this.rangeValue = (ev as RangeCustomEvent).detail.value;
+  }
   handleFilter(event: any) {}
+  handleFilterHamburguesas(event: any) {}
 }
