545dee9ca8eb98649f7863fa399cce109af74152
eldidac <eldidac@gmail.com>
2022-09-13 21:25:38 -0300

    fisrt commit

--- src/app/pages/usuario-cli-registro/usuario-cli-registro.page.ts
+++ src/app/pages/usuario-cli-registro/usuario-cli-registro.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-usuario-cli-registro',
@@ -6,8 +11,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./usuario-cli-registro.page.scss'],
 })
 export class UsuarioCliRegistroPage implements OnInit {
-
-  constructor() { }
+  form:FormGroup;
+  errorControls:any;
+  loading:boolean= false;
+  constructor(
+    private angularFireAuth: AngularFireAuth,
+    private formBuilder:FormBuilder,
+    // private loginService: LoginService,
+    private router:Router
+  ) {
+    this.form = this.formBuilder.group({
+      email:['', Validators.required],
+      password:['', Validators.required]
+    })
+    this.errorControls = this.form.controls;
+   }
 
   ngOnInit() {
   }
