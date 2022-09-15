5a0328ae865045a1a21927dcfe63a707a29aca0a
Daniel Medina <danielmedina012@gmail.com>
2022-09-14 23:44:43 -0300

    changes

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Categorias, Tamanios } from 'src/app/constants/constants';
 import { ICategoria, IProducto, IUsuario } from 'src/app/constants/interfaces';
 import { FirestoreBaseService } from 'src/app/services/firestore-base.service';
-import { first } from 'rxjs/operators';
+import { first, map } from 'rxjs/operators';
 import { UsuarioService } from 'src/app/services/usuario/usuario.service';
 import { Router } from '@angular/router';
 import { ProductoService } from 'src/app/services/producto/producto.service';
@@ -41,29 +41,34 @@ export class InicioPage implements OnInit {
     this.categoriesService.getCategoriesId().subscribe((resp) => {
       this.categorias = resp;
     });
-    this.productoService
-      .getProductsId()
-      .subscribe((resp) => {
-        this.productos = resp;
-        this.productoService.setProducts$(resp);
-      });
+    this.productoService.getProductsId().subscribe((resp) => {
+      this.productos = resp;
+      this.productoService.setProducts$(resp);
+    });
   }
 
   handleFilter(ev: Event) {
-    let event= ev as RadioGroupCustomEvent;
+    let event = ev as RadioGroupCustomEvent;
     this.catFiltrada = event.detail.value;
-    this.productoService.getProductsId({
-      where: [{ name: 'idCategoria', value: this.catFiltrada }],
-    }).subscribe((res)=>{
-      this.productoService.setProducts$(res)
-    });
+    this.productoService
+      .getProductsId({
+        where: [{ name: 'idCategoria', value: this.catFiltrada }],
+        order: 'nombre',
+      })
+      .subscribe((res) => {
+        res = res.filter(
+          (value, index, self) =>
+            index === self.findIndex((t) => t.nombre === value.nombre)
+        );
+        this.productoService.setProducts$(res);
+      });
   }
 
   selectProduct(id: string) {
     this.router.navigate(['/producto/', id]);
   }
-  getCategoriaProducto(id: string):string {
-    const desc= this.categorias.find((c)=>c.id===id)?.descCategoria
-    return desc?desc:"Sin categoria";
+  getCategoriaProducto(id: string): string {
+    const desc = this.categorias.find((c) => c.id === id)?.descCategoria;
+    return desc ? desc : 'Sin categoria';
   }
 }
