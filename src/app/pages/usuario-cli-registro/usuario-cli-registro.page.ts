c0e761438bed091447c2e55a6e36f090762a83f1
eldidac <eldidac@gmail.com>
2022-09-21 20:20:38 -0300

    registro email y pass andando

--- src/app/pages/usuario-cli-registro/usuario-cli-registro.page.ts
+++ src/app/pages/usuario-cli-registro/usuario-cli-registro.page.ts
@@ -28,9 +28,9 @@ export class UsuarioCliRegistroPage implements OnInit {
       fechaNac: ['', Validators.required],
       domiCalle: ['', Validators.required],
       domiNro: ['', Validators.required],
-      domiPiso: ['', Validators.required],
-      domiDpto: ['', Validators.required],
-      domiObs: ['', Validators.required],
+      domiPiso: ['',],
+      domiDpto: ['',],
+      domiObs: ['',],
       domiProv: ['', Validators.required],
       domiLoc: ['', Validators.required]
     })
@@ -50,7 +50,61 @@ export class UsuarioCliRegistroPage implements OnInit {
   }
 
   registrarEmail(){
-    this.modal.dismiss(null, 'cancel');
+    let email = this.formAuth.controls.email.value;
+    let password = this.formAuth.controls.password.value;
+    console.log("email: ", email, ", password: ", password);
+    this.angularFireAuth.createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Signed in
+      console.log("Usuario creado")
+      this.modal.dismiss(null, 'cancel');
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      //fnErrorDialog(errorCode, email, password)
+      console.log('Código error: ' + errorCode + ' / Mensaje: ' + errorMessage);
+    });
   }
 
+  /*
+  fnErrorDialog(){
+    if (errorCode == 'auth/email-already-in-use') {
+      var txt = 'El/la usuario/a '+email+' ya existe';
+      app.dialog.alert(txt, 'Un momento...');
+    }
+    
+    if (errorCode == 'auth/weak-password') {
+      var txt = 'La clave es muy débil. Intente utilizando más de 8 letras o números, incluyendo mayúsculas/minúsculas';
+      app.dialog.alert(txt, 'Un momento...');
+    }
+  
+    if (errorCode == 'auth/operation-not-allowed') {
+      var txt = 'Debe ingresar un e-mail y una contraseña';
+      app.dialog.alert(txt, 'Un momento...');
+    }
+   
+    if (errorCode == 'auth/invalid-email') {
+      var txt = 'La dirección de correo no es válida';
+      app.dialog.alert(txt, 'Un momento...');
+    }
+  
+    if (errorCode == 'auth/user-disabled') {
+      var txt = 'El usuario '+email+' ha sido desabilitado';
+      app.dialog.alert(txt, 'Un momento...');
+    }
+    
+    if (errorCode == 'auth/user-not-found') {
+      var txt = 'El usuario con el email '+email+' no existe';
+      app.dialog.alert(txt, 'Un momento...');
+    }
+  
+    if (errorCode == 'auth/wrong-password') {
+      var txt = 'La contraseña es incorrecta';
+      app.dialog.alert(txt, 'Un momento...');
+    }
+  }
+  */ 
+
 }
+
